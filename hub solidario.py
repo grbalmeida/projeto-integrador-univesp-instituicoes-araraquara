070c41ab7a04839e4d0f9c8df556b9d7d3bbf7c4
Felipe <26268554+Felipe-SM@users.noreply.github.com>
2021-09-27 21:46:15 -0300

    alterando front

--- hub solidario.py
+++ hub solidario.py
@@ -1,20 +1,23 @@
-from flask import Flask, redirect, url_for, render_template, request, session
-import sqlite3
+from flask import Flask, redirect, url_for, render_template, session
+import requests
+import json
 
 app = Flask(__name__)
 
 def db_connection():
     conn = None
-    conn = sqlite3.connect("instituicoes.db")
 
 @app.route("/home")
 @app.route("/")
 def home():
-    return render_template ("home.html")
+    imgs = requests.get("http://localhost:3000/api/imgs")
+    print(json.loads(imgs.content))
+    return render_template ("home.html", imgs = json.loads(imgs.content))
 
 @app.route("/instituicoes")
 def instituicoes():
-    return render_template("instituicoes.html")
+    instituicoes = requests.get("http://localhost:3000/api/instituicoes")
+    return render_template("instituicoes.html", instituicoes = json.loads(instituicoes.content))
 
 if __name__ == "__main__":
     app.run(debug=True)
