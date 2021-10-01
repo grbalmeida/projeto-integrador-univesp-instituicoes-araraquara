5108aa2a54df286db358aea569f92883ec57a787
Felipe <26268554+Felipe-SM@users.noreply.github.com>
2021-09-30 21:00:25 -0300

    dia 30.09

--- hub solidario.py
+++ hub solidario.py
@@ -1,6 +1,8 @@
-from flask import Flask, redirect, url_for, render_template, session
+from flask import Flask, redirect, url_for, render_template, session, request
 import requests
 import json
+
+# from requests.api import request
 
 app = Flask(__name__)
 
@@ -12,12 +14,34 @@ def db_connection():
 def home():
     imgs = requests.get("http://localhost:3000/api/imgs")
     print(json.loads(imgs.content))
-    return render_template ("home.html", imgs = json.loads(imgs.content))
+    return render_template ("home.html", imgs = json.loads(imgs.content), mostrar_categorias = False)
 
 @app.route("/instituicoes")
 def instituicoes():
-    instituicoes = requests.get("http://localhost:3000/api/instituicoes")
-    return render_template("instituicoes.html", instituicoes = json.loads(instituicoes.content))
+
+    nome = request.args.get('nome')
+
+    if not nome:
+        instituicoes = json.loads(requests.get("http://localhost:3000/api/instituicoes").content)
+    else:
+        instituicoes = json.loads(requests.get("http://localhost:3000/api/instituicoes?name_like=" + nome).content)
+    
+    categorias = json.loads(requests.get("http://localhost:3000/api/categorias").content)
+    
+    return render_template("instituicoes.html", instituicoes = instituicoes, categorias = categorias, mostrar_categorias = True, nome=nome)
+
+@app.route("/instituicoes/<categoria_id>")
+def instituicoes_id(categoria_id):
+    categorias = json.loads(requests.get("http://localhost:3000/api/categorias").content)
+    instituicoes = json.loads(requests.get("http://localhost:3000/api/instituicoes?categoria_id=" + categoria_id).content)
+    return render_template("instituicoes.html", instituicoes = instituicoes, categorias = categorias, mostrar_categorias = True)
+
+# @app.route("/instituicoes?nome=<nome>")
+# def instituicoes_nome(nome):
+#     categorias = json.loads(requests.get("http://localhost:3000/api/categorias").content)
+#     instituicoes = json.loads(requests.get("http://localhost:3000/api/instituicoes?name_like=" + nome).content)
+#     print (nome)
+#     return render_template("instituicoes.html", instituicoes = instituicoes, categorias = categorias, mostrar_categorias = True)
 
 if __name__ == "__main__":
     app.run(debug=True)
